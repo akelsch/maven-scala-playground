2710cffc42e3d5ba30c539d0fe772661a96bb5c9
Arthur Kelsch <akelsch@users.noreply.github.com>
2023-05-15 13:34:34 +0200

    Use explicit main method in SparkApp

--- spark/src/main/scala/com/example/SparkApp.scala
+++ spark/src/main/scala/com/example/SparkApp.scala
@@ -4,22 +4,27 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
 // Add VM option for Java 17: --add-exports java.base/sun.nio.ch=ALL-UNNAMED
-object SparkApp extends App {
+object SparkApp {
 
-  val spark = SparkSession.builder()
+  private val spark = SparkSession.builder()
     .appName("Simple Application")
     .master("local[*]")
     .getOrCreate()
 
   spark.sparkContext.setLogLevel("WARN")
 
-  val csvFile = Thread.currentThread().getContextClassLoader.getResource("people.csv")
-  val people = spark.read.option("header", "true").csv(csvFile.toString)
-  people.show()
+  def main(args: Array[String]): Unit = {
+    val csvPath = getClass.getResource("/people.csv").getPath
+    val people = spark.read
+      .option("header", "true")
+      .csv(csvPath)
+    people.show()
 
-  val newPeople = people.withColumn("fullname", concat_ws(", ", people("firstname"), people("lastname")))
-    .drop("firstname", "lastname")
-  newPeople.show()
+    val newPeople = people
+      .withColumn("fullname", concat_ws(", ", col("firstname"), col("lastname")))
+      .drop("firstname", "lastname")
+    newPeople.show()
 
-  spark.stop()
+    spark.stop()
+  }
 }
